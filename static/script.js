30e114b0c2ed7c6fcf29bc9492c07bb159fa6e87
Mustafa Damour <105155450+mustafa-damour@users.noreply.github.com>
2024-10-16 16:51:41 +0300

    Fixed CSS

--- static/script.js
+++ static/script.js
@@ -1,3 +1,5 @@
+const container = document.getElementById("dashboard");
+
 function validateForm() {
     let handle = document.forms["loginForm"]["handle"].value;
     let password = document.forms["loginForm"]["password"].value;
@@ -5,8 +7,11 @@ function validateForm() {
     if (handle.length <= 3) {
       alert("Name must be more than 3 letters.");
       return false;
-    } else if (password.length <= 8) {
-        alert("Name must be more than 8 chracters/numbers.");
+    } else if (password.length < 8) {
+        alert("Name must be at least 8 chracters/numbers.");
         return false;
       }   
 }
+
+
+container.addEventListener('')
