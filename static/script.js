412c26ea3d5e77c05c91d00caf58863e914d69b3
Mustafa-Damour <damour91919@gmail.com>
2024-10-18 19:12:28 +0300

    UPDATED Dashboard

--- static/script.js
+++ static/script.js
@@ -1,4 +1,3 @@
-const container = document.getElementById("dashboard");
 
 function validateForm() {
     let handle = document.forms["loginForm"]["handle"].value;
@@ -16,8 +15,45 @@ function validateForm() {
 
 
 
+const data = {
+  uid: 238746,
+  cid: 2342,
+  role: "User",
+  handle: "marco",
+  name: "Mark Twain",
+  email: "marco123@aio.jp"
 
+};
 
-function injectUserCard(id, data) {
-  
+function injectUserCard() {
+  let cardId = data.cid;
+  let userId = data.uid;
+  let role = data.role;
+  let handle = data.handle;
+  let name = data.name;
+  let email = data.email;
+  let inner = `
+      <div class="card-header">
+      <span class="role">${role}</span>
+        <span class="handle">@${handle}</span>
+      </div>
+      <div class="contacts-container">
+
+        <span class="contact">${name}</span>
+        <span class="contact">${email}</span>
+      </div>
+`;
+  let cardContainer = document.createElement("div");
+  cardContainer.className = "card-container";
+  cardContainer.id = `c-u${userId}-${cardId}`;
+  cardContainer.innerHTML = inner;
+  dashboard.appendChild(cardContainer);
 }
+
+function refresh(){
+  const dashboard = document.getElementById("dashboard");
+  injectUserCard();
+  console.log("User card is allegedly inject into the dashboard");
+}
+
+dashboard.onload = function() {refresh();}
