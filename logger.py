ef9ea1cbb0e5a7c0d6cbe998e5b390a82bc68342
Mustafa Damour <105155450+mustafa-damour@users.noreply.github.com>
2024-10-21 09:37:32 +0300

    COMMENTED the logger.py file

--- logger.py
+++ logger.py
@@ -3,18 +3,21 @@ from datetime import datetime as dt
 class Logger:
     def __init__(self, log_path:str='logs.txt'):
         self.log_path = log_path
-    
+        
+    # writing into the logs file
     def log(self, message:str):
         f = open(self.log_path, 'a')
         f.write(message+' '+dt.now().strftime("%m/%d/%Y, %H:%M:%S.%f")+' \n')
         f.close()
-        
+    
+    # fetching all logs 
     def get_logs(self):
         f = open(self.log_path, 'r')
         logs = f.read()
         f.close()
         return logs
     
+    #fetching last n logs
     def get_last_n_logs(self,n:int=1):
         f = open(self.log_path, 'r')
         logs = f.read().strip()
